25e4889ba2c11c8f970e8cd7be5f343c6de48058
Danil Pyatnitsev <danil@pyatnitsev.ru>
2025-02-19 18:08:26 +0700

    fix support of jms serializer newer than 3.29.1, now this library required doctrine/annotations.

--- src/Dto/JobDto.php
+++ src/Dto/JobDto.php
@@ -101,7 +101,7 @@ class JobDto
      * @Type("array")
      * @SerializedName("processors")
      */
-    private ?array $processors = null;
+    private ?array $processors = [];
 
     /**
      * @Type("int")
